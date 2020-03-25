535b2b297a23cfd1858fcd991d1fcd4f92b4fbd3
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-03-25 00:51:07 -0500

    initial commit, db and config added

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Bacon BUrger", 0);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Avocado BLT", 0);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Ham & Cheese", 1);
+
+SELECT * FROM burgers_db.burgers;
