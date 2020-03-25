ced69e81a6ad9b0aedf9a80cbff3c2508c41fd1d
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-03-25 01:13:39 -0500

    controllers file complete

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-const connection = require("connection.js");
+const connection = require("../config/connection.js");
 
 
 function printQuestionMarks(num) {
@@ -11,30 +11,22 @@ function printQuestionMarks(num) {
     return arr.toString();
 }
 
-// Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
     var arr = [];
 
-    // loop through the keys and push the key/value as a string int arr
     for (var key in ob) {
         var value = ob[key];
-        // check to skip hidden properties
+
         if (Object.hasOwnProperty.call(ob, key)) {
-            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
             if (typeof value === "string" && value.indexOf(" ") >= 0) {
                 value = "'" + value + "'";
             }
-            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-            // e.g. {sleepy: true} => ["sleepy=true"]
             arr.push(key + "=" + value);
         }
     }
 
-    // translate array of strings to a single comma-separated string
     return arr.toString();
 };
-
-
 
 
 var orm = {
@@ -45,25 +37,26 @@ var orm = {
         });
 
     },
+
     insertOne: function (table, cols, vals, cb) {
         const query = `INSERT INTO ${table} (${cols.toString()}) VALUES (${printQuestionMarks(vals.length)})`;
         console.log(query);
-        connection.query(query, vals, function(err, result) {
-            if(err) throw err;
+        connection.query(query, vals, function (err, result) {
+            if (err) throw err;
             cb(result);
         });
 
     },
+    
     updateOne: function (table, objColVals, condition, cb) {
         const query = `UPDATE ${table} SET ${objToSql(objColVals)} WHERE ${condition}`;
 
         console.log(query);
 
-        connection.query(query, function(err, result) {
-            if(err) throw err;
+        connection.query(query, function (err, result) {
+            if (err) throw err;
             cb(result);
         });
-
     }
 };
 
