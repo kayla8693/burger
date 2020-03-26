ddf1ba31d4ff74fd6a4b1633ec7eb36aa2567a90
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-03-25 23:47:29 -0500

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Inuyasha1",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Inuyasha1",
+        database: "burgers_db"
+    });
+}
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log('You are connected as id: ' + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
